14f2f11932b3e518e54c53d5921e91ed69405f39
Jacob Callear <jacob18@callear.net>
2020-12-29 20:00:09 +0000

    Add sudden death tournament script

---
+++ prisoners_dilemma/tournament_sudden_death.py
@@ -0,0 +1,68 @@
+'''Drop lowest scoring strategy in successive tournaments to find the winner.
+'''
+import logging
+logging.basicConfig(level=logging.WARNING)
+from collections import defaultdict
+from itertools import combinations
+from random import choice
+
+from prisoners_dilemma import strategies
+from prisoners_dilemma.play import play_game
+from prisoners_dilemma.print_table import convert_to_table, sort_by_value
+
+STRATEGIES_LIST = [
+    strategies.SneakyTitForTat,
+    strategies.TitForTat,
+    strategies.TitForTwoTats,
+    strategies.TitForTatPatterns,
+    strategies.HelpTheHelpers,
+    strategies.Unforgiving,
+    strategies.Equality,
+    strategies.ForgivingTitForTat,
+    strategies.GoByMajority,
+    # These four strategies are always the worst, well below the others
+    strategies.AlwaysCooperate,
+    strategies.AlwaysDefect,
+    strategies.Random,
+    strategies.TatForTit
+]
+
+counter = 1
+while len(STRATEGIES_LIST) > 1:
+    TOTAL_SCORES = defaultdict(int)
+
+    # Play all combinations of strategies
+    for strategy_1, strategy_2 in combinations(STRATEGIES_LIST, 2):
+        play_game(strategy_1, strategy_2, counter=TOTAL_SCORES)
+
+    # Play each strategy against itself
+    for strategy in STRATEGIES_LIST:
+        play_game(strategy, strategy, counter=TOTAL_SCORES)
+
+    # ==============================
+    # Remove lowest score
+    loser = min(
+        TOTAL_SCORES.items(),
+        key=lambda x: TOTAL_SCORES[x[0]]
+    )
+    # If multiple strategies had the same lowest score, drop one at random
+    losers = []
+    for name, score in TOTAL_SCORES.items():
+        if score == loser[1]:
+            losers.append((name, score))
+    if len(losers) > 1:
+        loser = choice(losers)
+    # If all competitors have equal score, print all
+    if len(losers) == len(STRATEGIES_LIST):
+        break
+    loser_name = eval(f'strategies.{loser[0]}')
+
+    STRATEGIES_LIST.pop(STRATEGIES_LIST.index(loser_name))
+
+    print(f'Round {counter} loser: {loser_name().__class__.__name__}')
+    counter += 1
+
+print(f'Round {counter} winner(s): ', [
+        class_name().__class__.__name__
+        for class_name in STRATEGIES_LIST
+    ])
